7f3699528e9fdb7a3fa17b23c7a3c7db8fb4760b
red-lexinus <starshenkov3@gmail.com>
2023-04-20 20:14:54 +0300

    create chapter_1/1.py

---
+++ Fluent_Python/chapter_1/1.py
@@ -0,0 +1,75 @@
+import random
+
+
+class GameCard:
+    _short_len = 13
+
+    def __init__(self, riddle: str, answer: str):
+        self._riddle, self._answer = riddle, answer
+
+    def __repr__(self):
+        return f"{self._short_riddle()}->{self._short_answer()}"
+
+    def __str__(self):
+        return f'{self._riddle}?'
+
+    def _short_riddle(self) -> str:
+        short_riddle = self._riddle
+        if len(self._riddle) > self._short_len - 3:
+            short_riddle = self._riddle[:10] + '...'
+        return short_riddle
+
+    def _short_answer(self) -> str:
+        short_answer = self._answer
+        if len(self._answer) > self._short_len - 3:
+            short_answer = self._answer[:10] + '...'
+        return short_answer
+
+    def get_answer(self) -> str:
+        return self._answer
+
+    def check_answer(self, answer: str) -> bool:
+        return bool(self._answer == answer)
+
+
+class GameDask:
+    def __init__(self, cards_dates: list[list]):
+        self._cards = [GameCard(*card_dates) for card_dates in cards_dates]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, pos: int) -> GameCard:
+        res = self._cards[pos]
+        del self._cards[pos]
+        return res
+
+
+def questionnaire(game_dask: GameDask, res: int = 0):
+    game_card: GameCard = random.choice(game_dask)
+    print(game_card)
+    if game_card.check_answer(input()):
+        print('Вы правы!')
+        res += 1
+    else:
+        print(f'Вы ошиблись вот правильный ответ:{game_card.get_answer()}')
+    if len(game_dask):
+        questionnaire(game_dask, res)
+    else:
+        return res
+
+
+def start_game():
+    cards_dates = [
+        ['1+1', '2'],
+        ['1+2', '3'],
+        ['2+3', '5']
+    ]
+    count_question = 3
+    game_dask = GameDask(cards_dates)
+    res = questionnaire(game_dask)
+    print(f"Всё наконец-то кончилось!\nВаши результаты:{res}/{count_question}")
+
+
+start_game()
+input()
